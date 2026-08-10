b23e2c2130c0c7161753013fc4d6c0c00d4b2b42
amberlyhw <isu10859004a@gmail.com>
2026-08-10 11:22:27 +0800

    Update service worker: bump cache to v2, add icon.svg

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'life-oso-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'life-oso-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
